c647b935e18e948e003173eaf749cdf4b250214f
Vikash Anand <vikashanandstu@gmail.com>
2018-06-23 17:22:42 -0700

    commit
    by Vikash

---
+++ SoftPACTesting/test/KDF/TransactionEntry.java
@@ -0,0 +1,47 @@
+package KDF;
+
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.*;
+
+public class TransactionEntry {
+	WebDriver driver;
+	String sheetname = "MembershipRegistration";
+	Sheet KDTexcelSheet;
+	String strDateFormat = "dd_MM_yyyy_HH_mm_ss";
+	SimpleDateFormat sdf = new SimpleDateFormat(strDateFormat);
+	java.util.Date date = new java.util.Date();
+	static HashMap<String, String> parameters = new HashMap<String, String>();
+	
+	@BeforeTest
+	public void setUp() throws Exception {
+		java.util.Date date = new java.util.Date();
+		System.out.println("\n Start Time - "+ sdf.format(date)+"\n\n");	
+		
+		LogRecorder.workingDir = "test\\resources\\data\\logRecord";
+		LogRecorder.fileName = "LOG_"+sheetname+"_"+sdf.format(date)+".xlsx";
+		LogRecorder.sheetName = sheetname;
+		LogRecorder.CreateExcel();
+		
+	}
+	
+	@DataProvider(name = "dataForTest")
+	public Object[][] getLoginData() {
+		return (ExcelFileSheet.readXLSX("test\\resources\\data","DDT.xlsx",sheetname));
+	}
+	
+	@Test//(dataProvider = "dataForTest")
+	public void registrationTest() throws Exception {
+		//TODO:pass parameters after making it ddt
+		KDTExecuter.executeTest(driver, sheetname, parameters);
+	}
+	
+	@AfterTest
+	public void tearDown() {
+		System.out.println("\n\nExecution Log - End Time - "+sdf.format(date));
+		
+	}
+}
