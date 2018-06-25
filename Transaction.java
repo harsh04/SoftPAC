10d84a7b34d96d148bc427d802da4d4a2daec526
Vikash Anand <vikashanand@users.noreply.github.com>
2018-06-25 05:47:01 -0700

    Created Transaction Java

---
+++ Transaction.java
@@ -0,0 +1,52 @@
+package KDF;
+
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.*;
+
+public class TransactionEntry {
+	WebDriver driver;
+	String sheetname = "transactionEntry";
+	Sheet KDTexcelSheet;
+	String strDateFormat = "dd_MM_yyyy_HH_mm_ss";
+	SimpleDateFormat sdf = new SimpleDateFormat(strDateFormat);
+	java.util.Date date = new java.util.Date();
+	static HashMap<String, String> parameters = new HashMap<String, String>();
+	
+	@BeforeTest
+	public void setUp() throws Exception {
+		java.util.Date date = new java.util.Date();
+		System.out.println("\n Start Time - "+ sdf.format(date)+"\n\n");	
+		
+		LogRecorder.workingDir = "test\\resources\\data\\logRecord";
+		LogRecorder.fileName = "LOG_"+sheetname+"_"+sdf.format(date)+".xlsx";
+		LogRecorder.sheetName = sheetname;
+		LogRecorder.CreateExcel();
+		
+	}
+	
+	@DataProvider(name = "dataForTest")
+	public Object[][] getTransactionData() {
+		return (ExcelFileSheet.readXLSX("test\\resources\\data","DDT.xlsx",sheetname));
+	}
+	
+	@Test(dataProvider = "dataForTest")
+	public void transactionTest(String memId, String accNo,String txnType, String instrument,String txnCode, String amt) throws Exception {
+		parameters.put("memberId", memId);
+		parameters.put("accountNo", accNo);
+		parameters.put("transactionType", txnType);
+		parameters.put("instrumentType", instrument);
+		parameters.put("transactionCode", txnCode);
+		parameters.put("amount", amt);
+		KDTExecuter.executeTest(driver, sheetname, parameters);
+	}
+	
+	@AfterTest
+	public void tearDown() {
+		System.out.println("\n\nExecution Log - End Time - "+sdf.format(date));
+
+	}
+}
